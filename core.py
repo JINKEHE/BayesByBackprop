3d66c4ea85ce00d17239c12caefce7c4f701303a
Jinke He <jinke@Jinkes-MacBook-Pro.local>
2019-02-24 21:01:12 +0000

    put bayesian networks into python file

---
+++ core.py
@@ -0,0 +1,205 @@
+import math
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.utils.data
+from torch import optim
+from torch import nn
+from torch import distributions as dist
+from enum import Enum
+
+# Default gaussian mixture parameters
+PI = 0.5
+SIGMA_1 = torch.tensor([math.exp(-0)])
+SIGMA_2 = torch.tensor([math.exp(-6)])
+
+# Default gaussian parameters
+MU_PRIOR = 0
+SIGMA_PRIOR = torch.tensor([math.exp(-0)])
+
+# Loss variance
+SIGMA = torch.tensor([math.exp(-2)])
+
+class PriorType(Enum):
+  MIXTURE = 1
+  GAUSSIAN = 2
+
+class ActivationType(Enum):
+  NONE = 0
+  RELU = 1
+  SOFTMAX = 2
+  TANH = 3
+  SIGMOID = 4
+
+class TaskType(Enum):
+  REGRESSION = 1
+  CLASSIFICATION = 2
+
+class GaussianMixture(object):
+
+  def __init__(self, pi, sigma1, sigma2):
+    self.pi = pi
+    self.sigma1 = sigma1
+    self.sigma2 = sigma2
+
+  def log_prob(self, weights):
+    normal_density1 = torch.exp(
+        dist.Normal(0.0, self.sigma1).log_prob(weights))
+    normal_density2 = torch.exp(
+        dist.Normal(0.0, self.sigma2).log_prob(weights))
+    return torch.sum(torch.log(
+        self.pi * normal_density1 +
+        (1 - self.pi) * normal_density2))
+
+class BayesianLayer(nn.Module):
+
+  def __init__(self,
+               input_size,
+               output_size,
+               prior_type=PriorType.MIXTURE,
+               prior_params={'pi' : PI, 'sigma1' : SIGMA_1, 'sigma2' : SIGMA_2},
+               activation_type=ActivationType.NONE,
+              ):
+    super().__init__()
+    self.input_size = input_size
+    self.output_size = output_size
+    self.activation_type = activation_type
+    self.mu_weights = nn.Parameter(torch.Tensor(output_size, input_size))
+    self.rho_weights = nn.Parameter(torch.Tensor(output_size, input_size))
+    self.mu_weights.data.uniform_(-0.5, 0.5)
+    self.rho_weights.data.uniform_(-4, -2)
+    self.mu_bias = nn.Parameter(torch.Tensor(output_size))
+    self.rho_bias = nn.Parameter(torch.Tensor(output_size))
+    self.mu_bias.data.uniform_(-0.5, 0.5)
+    self.rho_bias.data.uniform_(-4, -2)
+    if prior_type == PriorType.MIXTURE:
+      self.prior_weights = GaussianMixture(
+          prior_params['pi'], prior_params['sigma1'], prior_params['sigma2'])
+      self.prior_bias = GaussianMixture(
+          prior_params['pi'], prior_params['sigma1'], prior_params['sigma2'])
+    else:
+      self.prior_weights = dist.Normal(prior_params['mean'],
+                                       prior_params['sigma'])
+      self.prior_bias = dist.Normal(prior_params['mean'],
+                                    prior_params['sigma'])
+    self.log_prior = 0
+    self.log_posterior = 0
+
+  def _compute_gaussian_sample(self, mu, rho):
+    epsilon = dist.Normal(0, 1).sample(rho.size())
+    return mu + torch.log2(1 + torch.exp(rho)) * epsilon
+
+  def forward(self, input_data):
+    if self.training:
+      weights = self._compute_gaussian_sample(self.mu_weights, self.rho_weights)
+      bias = self._compute_gaussian_sample(self.mu_bias, self.rho_bias)
+      self.log_prior = (self.prior_weights.log_prob(weights).sum() +
+                        self.prior_bias.log_prob(bias).sum() )
+      sigma_weights = torch.log(1 + torch.exp(self.rho_weights))
+      sigma_bias = torch.log(1 + torch.exp(self.rho_bias))
+      self.log_posterior = (
+          dist.Normal(
+              self.mu_weights, sigma_weights).log_prob(weights).sum() +
+          dist.Normal(self.mu_bias, sigma_bias).log_prob(bias).sum()
+      )
+      if torch.isnan(self.log_posterior):
+        print('Weights log prob: ')
+        print( dist.Normal(
+              self.mu_weights, sigma_weights).log_prob(weights).sum())
+        print('Bias log prob: ' )
+        print(dist.Normal(self.mu_bias, sigma_bias).log_prob(bias).sum())
+    else:
+      weights = self.mu_weights
+      bias = self.mu_bias
+    linear_output = nn.functional.linear(input_data, weights, bias)
+    output = linear_output
+    if self.activation_type == ActivationType.RELU:
+      output = torch.relu(linear_output)
+    elif self.activation_type == ActivationType.SOFTMAX:
+      output = torch.softmax(linear_output)
+    elif self.activation_type == ActivationType.SIGMOID:
+      output = torch.sigmoid(linear_output)
+    elif self.activation_type == ActivationType.TANH:
+      output = torch.tanh(linear_output)
+    elif self.activation_type == ActivationType.NONE:
+      output = linear_output
+    else:
+      raise NotImplementedError
+    return output
+
+  def extra_repr(self):
+    return 'Bayesian Layer, in_size:{}, out_size:{}, activation_type:{}'.format(
+      self.input_size, self.output_size, self.activation_type.name
+    )
+
+class BayesianNN(nn.Module):
+
+  def __init__(
+      self,
+      nn_input_size,
+      layer_config=[100, 100, 10],           # list of layer output sizes
+      activation_config=[ActivationType.RELU, ActivationType.RELU, ActivationType.NONE],
+      prior_type=PriorType.MIXTURE,
+      prior_params={'pi' : PI, 'sigma1' : SIGMA_1, 'sigma2' : SIGMA_2},
+      task_type=TaskType.REGRESSION,         # determines the likelihood form
+  ):
+    super().__init__()
+
+    self.layers = nn.ModuleList([]) # ensures that all params are registered
+    self.input_size = nn_input_size
+    for i, output_size in enumerate(layer_config):
+      if i == 0:
+        input_size = self.input_size
+      else:
+        input_size = layer_config[i-1]
+
+      bayesian_layer = BayesianLayer(input_size, output_size,
+                                     activation_type = activation_config[i],
+                                     prior_type=prior_type,
+                                     prior_params=prior_params)
+      self.layers.append(bayesian_layer)
+    self.task_type = task_type
+
+  def forward(self, input_data):
+    current_data = input_data
+    for layer in self.layers:
+      current_data = layer.forward(current_data)
+    return current_data
+
+  def log_prior(self):
+    log_prior = 0
+    for layer in self.layers:
+      log_prior += layer.log_prior
+    return log_prior
+
+  def log_posterior(self):
+    log_posterior = 0
+    for layer in self.layers:
+      log_posterior += layer.log_posterior
+    return log_posterior
+
+  def cost_function(self, inputs, targets, num_samples, num_batches, k):
+    sum_log_posterior = 0
+    sum_log_prior = 0
+    sum_negative_log_likelihood = 0
+    for n in range(num_samples):
+      outputs = self(inputs)
+      sum_log_posterior += self.log_posterior()
+      sum_log_prior += self.log_prior()
+      if self.task_type == TaskType.CLASSIFICATION:
+        negative_log_likelihood = nn.functional.nll_loss(outputs, targets)
+      elif self.task_type == TaskType.REGRESSION:
+         negative_log_likelihood = - dist.Normal(
+             targets, SIGMA).log_prob(outputs).sum()
+      sum_negative_log_likelihood += negative_log_likelihood
+    kl_divergence = (sum_log_posterior / num_samples - sum_log_prior / num_samples) / num_batches
+    negative_log_likelihood = sum_negative_log_likelihood / num_samples
+    loss =  kl_divergence + negative_log_likelihood
+    return loss, kl_divergence, negative_log_likelihood
+
+  def extra_repr(self):
+    repr = ''
+    for layer in self.layers:
+      repr += layer.extra_repr()
+      repr += '\n'
+    return repr
