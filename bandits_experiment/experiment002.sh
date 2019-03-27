df2b4d99604c56f643a676ffa95a92eab43f949a
Andreea <andreeaganciulescu@gmail.com>
2019-03-27 17:39:57 +0200

    Add sh script for running bandits experiment

---
+++ bandits_experiment/experiment002.sh
@@ -0,0 +1,13 @@
+cd ..
+python3 bandits_experiment.py \
+     --experiment_name Experiment002 \
+     --optimizer_type  Adam \
+     --eg_learning_rate 1e-3 \
+     --eg_epsilon 1e-3 \
+     --bnn_learning_rate 1e-3 \
+     --bnn_epsilon 1e-3 \
+     --bnn_lr_scheduler_step_size 32 \
+     --bnn_pi 0.75 \
+     --bnn_log_sigma1 -3 \
+     --bnn_log_sigma2 -8 
+      
