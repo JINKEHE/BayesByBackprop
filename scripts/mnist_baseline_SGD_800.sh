d4dbd524e40d451701295be20f667369e807f99c
Jinke He <jinkehe1996@gmail.com>
2019-04-13 18:47:47 +0100

    removed old scripts/results; further cleaning up

--- scripts/mnist_baseline_SGD_800.sh
+++
@@ -1,8 +0,0 @@
-cd ../src/mnist/
-python mnist_experiment.py \
-    --num_epochs 600 \
-    --num_units 800 \
-    --lr 1e-3 \
-    --batch_size 128 \
-    --network_type standard \
-    --optimizer SGD \
